f82ac84e14c990e19b1e5294fae51945e89b5f1a
Aaron Nowack <aaronbnowack@gmail.com>
2015-02-27 13:16:00 -0500

    working cylinder setup

--- cylinder_run.py
+++ cylinder_run.py
@@ -3,6 +3,7 @@
 Created on Thu Feb 26 12:49:11 2015
 
 @author: Aaron
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,69 +11,79 @@ from mpl_toolkits.mplot3d import Axes3D
 from mcnp_wrapper import *
 from fission_display import *
 
-def radial_bins(r, n):
-    c = r / (n**(1./3.))
-    bins = np.ndarray((n+1))
-    for i in xrange(len(bins)):
-        bins[i] = c * np.power(i, 1./3.)
-    return bins
+def radial_bins(r_min, r_max, n):
+    bins = np.arange(n+1)
+    bins = float(r_min)**2 + bins*(float(r_max)**2-float(r_min)**2)/float(n)
+    return np.sqrt(bins)
 
-def plot_radial_bins(r, radius, nbins, nps, *args, **kwargs):
-    plot_args = {'color': 'blue', 'alpha': 0.5, 'linewidth': 2}
-    eplot_args = {'color': 'blue', 'alpha': 0.5, 'linewidth': 2,
-                  'ecolor': 'blue'}
-                  
-    for key, val in kwargs.iteritems():
-        if plot_args.has_key(key):
-            plot_args[key] = val
-            
-    for key, val in kwargs.iteritems():
-        if eplot_args.has_key(key):
-            eplot_args[key] = val
-            
-    ubins = uniform_bins(radius, nbins)
-    vals = np.histogram(r, bins=ubins)[0].astype(float)
-    error = np.sqrt(vals) / float(nps) * nbins
-    norm_vals = vals / float(nps) * nbins
-    
-    plt.plot(ubins, np.concatenate([norm_vals, [norm_vals[-1]]]), drawstyle='steps-post', **plot_args)    
-    plt.errorbar((ubins[1:]+ubins[:-1])/2., norm_vals, yerr=error, fmt='none', **eplot_args)
-
-    plt.ylim(ymin=0)
-    plt.show()
-
-input_card = \
-"""First Attempt at P Mapping Input Deck
+no_reflector_card = \
+"""Cylinder Assembly
 C CELL CARDS
-10 100 -18.74 -1
-20 0          -1
+10 100  -18.74  -1 2       $ CYLINDER SHELL
+20   0         (-3 1 ):-2  $ SURROUNDING MEDIUM
+30   0           3         $ NULL SPACE
 
-C SURFACE CARDS
-1 SO {RADIUS}
+1 RCC 0 0 0 0 0 10 5 $ OUTER
+2 RCC 0 0 0 0 0 10 4 $ INNER
+3 SO  100            $ UNIVERSE
 
-M100 92235 {F235} 92238 {F238}
+M100 92235 0.935 92238 0.065
 MODE N
-IMP:N 1 0
+IMP:N 1 1 0
 PHYS:N 100 0 0 J J J 0 -1 J J J 0 0
 FMULT 92235 METHOD=5
-NPS {NPS}
+NPS 500000
 PTRAC FILE=ASC WRITE=ALL MAX=50000000
-C UNFIROMLY SAMPLING IN SPHERE
-SDEF ERG=2.0 POS=0 0 0 RAD=D1 CEL=10
-SI1 0 {RADIUS}
+C UNFIROMLY SAMPLING IN CYLINDER
+SDEF ERG=2.0 AXS=0 0 1 POS=0 0 0 RAD=D1 EXT=D2 CEL=10
+SI1 4 5
 SP1 -21 2
-RAND SEED={SEED}
+SI2 0 10
+SP2 0 1
+RAND SEED=8675309
 """
 
-enrichment = [0.935, 0.90, 0.80, 0.70, 0.60]
-colors = ['b', 'g', 'r', 'c', 'm']
-params = {'NPS': 100000, 'RADIUS': 5.0, 'F235': 0.9, 'F238': 0.1, 
-          'SEED': 8675309}
-          
-n_radial_bins = 20
+reflector_card = \
+"""Cylinder Assembly
+C CELL CARDS
+10 100 -18.74  -1 2        $ CYLINDER ASSEMBLY
+20   0        (-3 1 4 ):-2 $ SURROUNDING MEDIUM
+30 200 -19.25  -4          $ BOX
+40   0          3          $ NULL SPACE
+
+1 RCC   0 0 0 0 0 10 5 
+2 RCC   0 0 0 0 0 10 4 
+3 SO  100 
+C 4 RPP  -5 5 5 6 0 10 
+4 RPP  -5 5 -6 -5 0 10 
+
+C ENRICHED URANIUM, DENSITY=18.74 G/CM^3
+M100 92235 0.935 92238 0.065
+C TUNGSTEN, DENSITY=19.25 G/CM^3
+M200 074180 0.0012
+     074182 0.2650
+     074183 0.1431
+     074184 0.3064
+     074186 0.2843
+MODE N
+IMP:N 1 1 1 0
+PHYS:N 100 0 0 J J J 0 -1 J J J 0 0
+FMULT 92235 METHOD=5
+NPS 500000
+PTRAC FILE=ASC WRITE=ALL MAX=50000000
+C UNFIORMLY SAMPLING IN CYLINDER
+SDEF ERG=2.0 AXS=0 0 1 POS=0 0 0 RAD=D1 EXT=D2 CEL=10
+SI1 4 5
+SP1 -21 2
+SI2 0 10
+SP2 0 1
+RAND SEED=8675309
+"""
+  
+n_radial_bins = 5
 n_phi_bins = 20
 
-radial_bins = radial_bins(params['RADIUS'], n_radial_bins)
+radial_bins = radial_bins(4., 5., n_radial_bins)
 phi_bins = np.linspace(0, 2*np.pi, n_phi_bins)
 radial_mesh, phi_mesh = np.meshgrid(radial_bins, phi_bins)
 
@@ -80,26 +91,16 @@ nbins = n_radial_bins * n_phi_bins
 
 fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
 
-with run_mcnp(input_card, params=params, cores=4) as (status, mcnp_dir):  
+with run_mcnp(no_reflector_card, params={}, cores=4) as (status, mcnp_dir):  
     fissions, nhistory = parse_ptrac_fissions(mcnp_dir + '\\ptrac')
-    r = np.sqrt(fissions[:, 0]**2. + fissions[:, 1]**2. + fissions[:, 2]**2.)
+    r = np.sqrt(fissions[:, 0]**2. + fissions[:, 1]**2.)
     phi = np.arctan2(fissions[:,1], fissions[:,0]) + np.pi
     
-    hist, xedges, yedges = np.histogram2d(r, phi, bins=[radial_bins, phi_bins])
-    
+    hist, xedges, yedges = np.histogram2d(r, phi, bins=[radial_bins, phi_bins]) 
     hist = hist / float(nhistory) * nbins
-    
-    print radial_mesh.shape, phi_mesh.shape, hist.shape
-    
+
     quadmesh = ax.pcolormesh(yedges, xedges, hist)
-    
-#    plt.plot(ubins, np.concatenate([norm_vals, [norm_vals[-1]]]), drawstyle='steps-post')
-    
     print fissions.shape[0] / float(nhistory)
 
-#plt.ylim(ymin=0)
-#plt.xlabel('Radius (cm)')
-#plt.ylabel('p (fission probability)')
-#plt.legend(loc='best')
 plt.colorbar(quadmesh,ax=ax)
 plt.show()
